b813cf71100ce68248061f5324fdedd9625e04a4
Asikur Rahaman <mdasikur5893@gmail.com>
2026-01-30 02:12:23 +0600

    odd numbers sum

--- script.js
+++ script.js
@@ -24,8 +24,22 @@ rules:1-divisible by 400 or
 2-divisible by 4 and not divisable by 100
 */
 function leapYear (year){
-    return  (year % 4 === 0 && year % 100 !== 0);
+    return (year % 400 === 0) || (year % 4 === 0 && year % 100 !== 0);
 }
 console.log(leapYear(2026));
 console.log(leapYear(2016));
 console.log(leapYear(1971));
+// sum of odd of an array
+function oddNumbers(numbers){
+    let oddNumber =[];
+    if (numbers % 2 !==0){
+        oddNumber.push(numbers)
+    }
+    let sum = 0;
+    for (num of oddNumber){
+        sum = sum + num;
+    }
+    return sum
+}
+const array = [1,3,20,5,11]
+console.log(oddNumbers(array));
