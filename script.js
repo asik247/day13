48a32d96c919bc23ab902de03eef8f33a5e7189a
Asikur Rahaman <mdasikur5893@gmail.com>
2026-01-30 01:19:14 +0600

    feet to inch and inch to fit

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 /* 1 foot = 12 inc
+ \(0.0833\)
 code start here: inc to fit:-
 
 */
-const foot=(inch)=>{
-    return inch / 12;
+const foot=(feet)=>{
+    return feet * 12;
 }
-console.log(foot(65));
+console.log(foot(5.7));
+
+
+
+
+const inch = (foot)=>{
+    // return foot*0.0833
+    return foot / 12
+
+}
+console.log(inch(68.4));
